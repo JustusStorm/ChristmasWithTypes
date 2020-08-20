2612f761825ce45a5fb4d4ae808f2ba5a7d08777
Justus Storm <storm.j.dev@gmail.com>
2020-08-20 15:22:36 -0500

    Changed all the variable types and other errors to allow program to run. USE THIS CODE AS A REFERENCE

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -11,10 +11,10 @@ namespace ChristmasWithTypes
             var xmasDay = Christmas.Day.Thursday;
 
             //TODO set Santa's name to Kris Kringle
-            xmas.Santa = null;
+            xmas.Santa = "Kris Kringle";
 
             //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{ }; 
+            xmas.Presents =  new string[3]{"Remington 1911 R1 ", "Martin Guitar", "Gaming Computer" }; 
 
             //TODO Set the TreeHeight to 10
             xmas.TreeHeight = null;
